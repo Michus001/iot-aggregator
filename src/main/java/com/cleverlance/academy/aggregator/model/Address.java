2c63c666edabe00d84586474bb0f0fc4b79af06d
tomas <tomas@michalicka.com>
2022-03-08 19:06:19 +0100

    4 cviceni - db init

--- src/main/java/com/cleverlance/academy/aggregator/model/Address.java
+++ src/main/java/com/cleverlance/academy/aggregator/model/Address.java
@@ -1,5 +1,6 @@
 package com.cleverlance.academy.aggregator.model;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import lombok.Data;
 import lombok.Value;
@@ -7,7 +8,10 @@ import lombok.Value;
 @Embeddable
 @Data
 public class Address {
+    @Column(name = "street")
     private String street;
+    @Column(name = "number")
     private String number;
+    @Column(name = "city")
     private String city;
 }
